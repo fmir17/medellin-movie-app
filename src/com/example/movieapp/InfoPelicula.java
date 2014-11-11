ce229153c0d53f70a81bc37773c0c005dc48c362
sebastian garcia <sebastiangarcia9201@gmail.com>
2014-11-11 12:06:17 -0500

    Formato de la activity listo

--- src/com/example/movieapp/InfoPelicula.java
+++ src/com/example/movieapp/InfoPelicula.java
@@ -11,31 +11,40 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class InfoPelicula extends Activity {
 
-	private TextView lblID, lblTitle, lblDescription, lblUrl, lblGenre,
-			lblFormat, lblDirector;
-
+	private TextView lblTitle, lblDescription,lblGenre,lblFormat, lblDirector;
+	private String descripcion;
+	//private WebView webView;
+	
+    static Point size;
+    static float density;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.info_pelicula);
 
-		lblID = (TextView) findViewById(R.id.lblID);
 		lblTitle = (TextView) findViewById(R.id.lblTitle);
-		lblDescription = (TextView) findViewById(R.id.lblDescription);
-		lblUrl = (TextView) findViewById(R.id.lblUrl);
+		//lblDescription = (TextView) findViewById(R.id.lblDescription);
 		lblGenre = (TextView) findViewById(R.id.lblGenre);
 		lblFormat = (TextView) findViewById(R.id.lblFormat);
 		lblDirector = (TextView) findViewById(R.id.lblDirector);
-
+		//webView = (WebView) findViewById(R.id.webView1);
+		      
+	
 		Bundle bundle = getIntent().getExtras();
-		lblID.setText(bundle.getString("identidad"));
 		
 		obtenerPelicula obtenerPel = new obtenerPelicula();
 		obtenerPel.execute(bundle.getString("identidad"));
@@ -78,20 +87,29 @@ public class InfoPelicula extends Activity {
 
 			return title;
 		}
-
+		
 		@Override
 		protected void onPostExecute(String result) {
-			// TODO Auto-generated method stub
+
 			super.onPostExecute(result);
 			try {
-				lblID.setText("" + respJSON.getString("id"));
 				lblTitle.setText("" + respJSON.getString("title"));
-				lblDescription.setText("" + respJSON.getString("description"));
-				lblUrl.setText("" + respJSON.getString("urlImage"));
+				//lblDescription.setText("" + respJSON.getString("description"));
 				lblGenre.setText("" + respJSON.getString("genre"));
 				lblFormat.setText("" + respJSON.getString("format"));
 				lblDirector.setText("" + respJSON.getString("director"));
-
+												
+				/*descripcion = "<html><body>"
+				+ "<p align=\"justify\">"                
+				+ respJSON.getString("description")
+				+ "</p> "
+				+ "</body></html>";	
+				webView.loadData(descripcion, "text/html", "utf-8");*/
+				descripcion = respJSON.getString("description");
+				JustifiedTextView jtv= new JustifiedTextView(getApplicationContext(), descripcion );
+				LinearLayout place = (LinearLayout) findViewById(R.id.LayoutPrincipal);
+				place.addView(jtv);
+				
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
